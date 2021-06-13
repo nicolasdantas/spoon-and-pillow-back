87be0637e61081103c67f538d5122fc8663e1bf9
Nicolas Dantas <nicolas.dantas@gmail.com>
2021-06-13 22:53:31 +0200

    init project

---
+++ src/contact.ts
@@ -0,0 +1,52 @@
+import SibApiV3Sdk from 'sib-api-v3-sdk';
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+import dotenv from 'dotenv';
+dotenv.config();
+
+const apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.SENDINBLUE_TOKEN;
+
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+const sendEmail = (req, res) => {
+  const data = req.body;
+  const dataToSend = {
+    sender: {
+      name: `${data.firstname} ${data.lastname}`,
+      email: data.email,
+    },
+    subject: 'Vous avez reçu un nouveau message depuis Spoon and Pillow',
+    htmlContent: `<html>
+				<head>
+				</head>
+						<body>
+							<h2>Bonjour Marion,</h2>
+							<p>Vous avez reçu un nouveau message de ${data.firstname} ${data.lastname} ($[])</p>
+							<p>Voici son contenu :</p>
+							<p>${data.message}</p>
+						</body>
+			</html>`,
+    to: [
+      {
+        email: 'nicolas.dantas@gmail.com',
+        name: 'Nicolas Dantas',
+      },
+    ],
+    params: {
+      name: 'John',
+      surname: 'Doe',
+    },
+  };
+  apiInstance.sendTransacEmail(dataToSend).then(
+    function (data: any) {
+      console.log('ok');
+    },
+    function (error: any) {
+      res.sendStatus(500);
+      throw new Error('error');
+    }
+  );
+};
+
+export { sendEmail };
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
-import mongoose from 'mongoose';
 import cors from 'cors';
 import { config, IConfig } from './config/environment.dev';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import { sendEmail } from './contact';
+import asyncHandler from 'express-async-handler';
 
 const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 const env: IConfig = config;
+dotenv.config();
 
 app.listen(env.serverPort, () => console.log(`server is running on port ${env.serverPort}`));
 
-// on définit la fonction connectToDb qu'on appelle immédiatement
-(async () => {
-  await mongoose.connect(env.db, env.options);
-  console.log(`MonboDB is running, using the connection ${env.db}`);
-})();
+app.get('/instagram', async (req, res) => {
+  const result: any = await axios.get(
+    `https://graph.instagram.com/17841445240374833/media?fields=id,caption,permalink,thumbnail_url,media_url&access_token=${process.env.INSTAGRAM_TOKEN}`
+  );
+  res.send(result.data);
+});
+
+app.post('/contact', asyncHandler(sendEmail));
